1cc840734f7d88ee29522f266bb141bcfc686282
Gourav sen <gouravsen757@gmail.com>
2025-01-30 19:56:05 +0530

    Update skip a no. in arrey.cpp

--- skip a no. in arrey.cpp
+++ skip a no. in arrey.cpp
@@ -1,8 +1,7 @@
 #include<iostream>
 using namespace std;
 int main (){
-	int arr[]={1,2,3,40,5,6};
-	
+	int arr[]={1,2,3,40,5,6};	
 	for(int i=0;i<=6;i++){
 		if(arr[i]!=40){
 			cout<<arr[i]<<endl;
